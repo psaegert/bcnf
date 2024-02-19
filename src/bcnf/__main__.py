1972961c86ddd4afee22030d5fd24c4b56702698
CKleiber <christian.kleiber01@gmail.com>
2024-02-19 16:03:15 +0100

    Implemented camera skeleton

--- src/bcnf/__main__.py
+++ src/bcnf/__main__.py
@@ -1,9 +1,12 @@
 import argparse
 import sys
 
-from bcnf.debug_plotting import debug_plotting
-from bcnf.observation import simple_2D_camera_observation
-from bcnf.physics import physics_ODE_simulation, physics_simulation
+import numpy as np
+
+from bcnf.camera import camera
+from bcnf.debug_plotting import show_camera_image  # , debug_plotting
+# from bcnf.observation import simple_2D_camera_observation
+from bcnf.physics import physics_ODE_simulation  # , physics_simulation
 
 
 def main(argv: str = None) -> None:
@@ -30,14 +33,23 @@ def main(argv: str = None) -> None:
         case 'demo':
             print(f'Running demo with dummy_option={args.dummy_option}')
         case 'test_physics':
-            print('Running physics simulation; Comparing simple physis with ODE integration...')
-            p1 = physics_simulation(x0=args.x0)
+            print('Running physics simulation; Comparing simple physics with ODE integration...')
+            # p1 = physics_simulation(x0=args.x0)
             p2 = physics_ODE_simulation(x0=args.x0)
 
-            p1 = simple_2D_camera_observation(p1)
-            p2 = simple_2D_camera_observation(p2, noise=True, std=0.05)
+            # p1 = simple_2D_camera_observation(p1)
+            # p2 = simple_2D_camera_observation(p2, noise=True, std=0.05)
 
-            debug_plotting(p1, p2)
+            img = camera(
+                ball_pos=p2[3],
+                ratio=(16, 9),
+                fov_horizontal=70.0,
+                cam_pos=np.array([-25, 0, 1.5]),
+            )
+
+            show_camera_image(img)
+
+            # debug_plotting(p1, p2)
         case _:
             print('Unknown command: ', args.command)
             sys.exit(1)
