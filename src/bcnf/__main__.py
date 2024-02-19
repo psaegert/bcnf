50a6008e9d350edc4b960fd11d63764a965dbbfc
CKleiber <christian.kleiber01@gmail.com>
2024-02-19 16:41:23 +0100

    Making videos of trajectory is implemented

--- src/bcnf/__main__.py
+++ src/bcnf/__main__.py
@@ -3,8 +3,8 @@ import sys
 
 import numpy as np
 
-from bcnf.camera import camera
-from bcnf.debug_plotting import show_camera_image  # , debug_plotting
+from bcnf.camera import record_trajectory  # camera
+# from bcnf.debug_plotting import show_camera_image, debug_plotting
 # from bcnf.observation import simple_2D_camera_observation
 from bcnf.physics import physics_ODE_simulation  # , physics_simulation
 
@@ -33,13 +33,19 @@ def main(argv: str = None) -> None:
         case 'demo':
             print(f'Running demo with dummy_option={args.dummy_option}')
         case 'test_physics':
-            print('Running physics simulation; Comparing simple physics with ODE integration...')
+            print('Running physics simulation; Recording Video...')
             # p1 = physics_simulation(x0=args.x0)
             p2 = physics_ODE_simulation(x0=args.x0)
 
             # p1 = simple_2D_camera_observation(p1)
             # p2 = simple_2D_camera_observation(p2, noise=True, std=0.05)
 
+            record_trajectory(p2, cam_pos=np.array([-25, 0, 1.5]), make_gif=True, gif_name='trajectory_cam1')
+            record_trajectory(p2, cam_pos=np.array([0, -25, 1.5]), make_gif=True, gif_name='trajectory_cam2')
+            record_trajectory(p2, cam_pos=np.array([25, 0, 1.5]), make_gif=True, gif_name='trajectory_cam3')
+            record_trajectory(p2, cam_pos=np.array([0, 25, 1.5]), make_gif=True, gif_name='trajectory_cam4')
+
+            '''
             img = camera(
                 ball_pos=p2[3],
                 ratio=(16, 9),
@@ -48,6 +54,7 @@ def main(argv: str = None) -> None:
             )
 
             show_camera_image(img)
+            '''
 
             # debug_plotting(p1, p2)
         case _:
--- src/bcnf/camera.py
+++ src/bcnf/camera.py
@@ -1,5 +1,46 @@
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+def record_trajectory(trajectory: np.ndarray = np.array([[0, 0, 0]]),
+                      ratio: tuple = (16, 9),
+                      fov_horizontal: float = 70.0,
+                      cam_pos: np.ndarray = np.array([-25, 0, 1.5]),
+                      make_gif: bool = True,
+                      gif_name: str = 'trajectory'
+                      ) -> np.ndarray:
+    aspect_ratio = ratio[0] / ratio[1]
+
+    fov_vertical = fov_horizontal / aspect_ratio
+
+    phi = (fov_horizontal / 2) * (np.pi / 180)
+    theta = (fov_vertical / 2) * (np.pi / 180)
+
+    film = []
+    for position in trajectory:
+        img = camera(
+            ball_pos=position,
+            ratio=ratio,
+            fov_horizontal=fov_horizontal,
+            cam_pos=cam_pos,
+        )
+        film.append(img)
+
+    film = np.array(film)
+
+    if make_gif:
+        fig, ax = plt.subplots(figsize=(ratio[0], ratio[1]))
+        ax.set_xlabel('horizontal angle')
+        ax.set_ylabel('vertical angle')
+        ax.title.set_text(f'{gif_name}')
+        gif = []
+        for i in range(trajectory.shape[0]):
+            gif.append([ax.imshow(film[i], extent=[-phi, phi, -theta, theta], cmap='hot', animated=True)])
+
+        ani = animation.ArtistAnimation(fig, gif, interval=100, blit=True, repeat_delay=3000)
+        ani.save(f'tests/gifs/{gif_name}.gif', writer='imagemagick')
+    return film
 
 
 def camera(ball_pos: np.ndarray = np.array([0, 0, 1.5]),
@@ -49,13 +90,20 @@ def camera(ball_pos: np.ndarray = np.array([0, 0, 1.5]),
 
     if show_plot:
         fig, ax = plt.subplots(figsize=(ratio[0], ratio[1]))
-        vals = ax.hist2d(ph, th, bins=[ratio[0] * 10, ratio[1] * 10], range=[[-phi, phi], [-theta, theta]], density=True, cmap='hot')
+        ax.hist2d(ph, th, bins=[ratio[0] * 10, ratio[1] * 10], range=[[-phi, phi], [-theta, theta]], density=True, cmap='hot')
         ax.set_xlabel('horizontal angle')
         ax.set_ylabel('vertical angle')
         plt.show()
 
+    vals = np.histogram2d(ph, th, bins=[ratio[0] * 10, ratio[1] * 10], range=[[-phi, phi], [-theta, theta]])
+
+    # check for NaNs if the hisogram was empty (ball outside of fov)
+    if np.any(vals[0]):
+        # Normalize the histogram
+        vals = vals[0] / np.sum(vals[0])
     else:
-        vals = np.histogram2d(ph, th, bins=[ratio[0] * 10, ratio[1] * 10], range=[[-phi, phi], [-theta, theta]], density=True)
+        # If histogram is empty, create an array filled with zeros of the same shape
+        vals = np.zeros_like(vals[0])
 
     # reverse and transpose vals[0] to return the correct orientation of the image
-    return np.flipud(vals[0].T)
+    return np.flipud(vals.T)
