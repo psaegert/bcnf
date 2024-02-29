891843c63f232c848cecf3e351bcb872ce45036f
psaegert <paulsaegert@protonmail.com>
2024-02-29 10:01:04 +0100

    Re-organize files
    - Fix missing scipy requirement
    - Move simulation and debug code to their own packages
    - Move experimental notebook to new experimental directory
    - This keeps the tests folder clean for actual pytests

--- src/bcnf/__main__.py
+++ src/bcnf/__main__.py
@@ -1,9 +1,9 @@
 import argparse
 import sys
 
-from bcnf.debug_plotting import debug_plotting
-from bcnf.observation import simple_2D_camera_observation
-from bcnf.physics import physics_ODE_simulation, physics_simulation
+from bcnf.debug.debug_plotting import debug_plotting
+from bcnf.simulation.observation import simple_2D_camera_observation
+from bcnf.simulation.physics import physics_ODE_simulation, physics_simulation
 
 
 def main(argv: str = None) -> None:
@@ -15,11 +15,11 @@ def main(argv: str = None) -> None:
     parser = argparse.ArgumentParser(description='Ballistic Conditional Normalizing Flows (BCNF)')
     subparsers = parser.add_subparsers(dest='command_name', required=True)
 
-    # answerQuestion
+    # TODO: Remove demo command
     demo_parser = subparsers.add_parser("demo")
-    test_physics_parser = subparsers.add_parser("test_physics")
-
     demo_parser.add_argument('--dummy_option', type=str, default='dummy_value', help='Dummy option')
+
+    test_physics_parser = subparsers.add_parser("test_physics")
     test_physics_parser.add_argument('--x0', type=float, nargs=3, default=[0, 0, 1.8], help='Initial position')
 
     # Evaluate input
