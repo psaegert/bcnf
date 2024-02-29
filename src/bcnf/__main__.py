f374346d148f75237ce16c33e1059ad8b1b0cfce
psaegert <paulsaegert@protonmail.com>
2024-02-29 10:03:33 +0100

    Move experimental physics test to notebook in experimental folder

--- src/bcnf/__main__.py
+++ src/bcnf/__main__.py
@@ -1,10 +1,6 @@
 import argparse
 import sys
 
-from bcnf.debug.debug_plotting import debug_plotting
-from bcnf.simulation.observation import simple_2D_camera_observation
-from bcnf.simulation.physics import physics_ODE_simulation, physics_simulation
-
 
 def main(argv: str = None) -> None:
     """
@@ -19,9 +15,6 @@ def main(argv: str = None) -> None:
     demo_parser = subparsers.add_parser("demo")
     demo_parser.add_argument('--dummy_option', type=str, default='dummy_value', help='Dummy option')
 
-    test_physics_parser = subparsers.add_parser("test_physics")
-    test_physics_parser.add_argument('--x0', type=float, nargs=3, default=[0, 0, 1.8], help='Initial position')
-
     # Evaluate input
     args = parser.parse_args(argv)
 
@@ -29,15 +22,6 @@ def main(argv: str = None) -> None:
     match args.command_name:
         case 'demo':
             print(f'Running demo with dummy_option={args.dummy_option}')
-        case 'test_physics':
-            print('Running physics simulation; Comparing simple physis with ODE integration...')
-            p1 = physics_simulation(x0=args.x0)
-            p2 = physics_ODE_simulation(x0=args.x0)
-
-            p1 = simple_2D_camera_observation(p1)
-            p2 = simple_2D_camera_observation(p2, noise=True, std=0.05)
-
-            debug_plotting(p1, p2)
         case _:
             print('Unknown command: ', args.command)
             sys.exit(1)
