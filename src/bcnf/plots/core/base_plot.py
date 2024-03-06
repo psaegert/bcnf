48f839d17918dc2b22fddd0b253a9c5ff7c7290f
MrWhatZitToYaa <ichich111222333@gmail.com>
2024-03-06 15:31:46 +0100

    Added save fig function

--- src/bcnf/plots/core/base_plot.py
+++ src/bcnf/plots/core/base_plot.py
@@ -1,10 +1,17 @@
-import pandas as pd
 from abc import ABC, abstractmethod
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 class BasePlot(ABC):
     def __init__(self, data: pd.DataFrame):
         self.data = data
+        self.fig: plt.Figure
 
     @abstractmethod
     def create_plot(self) -> None:
         pass
+
+    def save_plot(self, filepath: str, filename: str) -> None:
+        self.fig.savefig(filepath + filename)
