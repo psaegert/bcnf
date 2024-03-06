1c9bc5cdc5ef065d405dffcf3421f3994d7cfa68
MrWhatZitToYaa <ichich111222333@gmail.com>
2024-03-06 14:13:49 +0100

    Added two plot classes

---
+++ src/bcnf/plots/data/covariances.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class DataConvariancePlot:
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+
+    def plot(self):
+        pass
---
+++ src/bcnf/plots/data/data_distribution.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class DataDistributionPlot:
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+
+    def plot(self):
+        pass
