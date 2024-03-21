626558453002436d335935d11f4951ca8c5848f6
psaegert <paulsaegert@protonmail.com>
2024-03-21 21:16:28 +0100

    Add max video config

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
-bcnf train -c configs/runs/trajectory_LSTM_small.yaml
-bcnf train -c configs/runs/trajectory_LSTM_large.yaml
-bcnf train -c configs/runs/trajectory_TRF_small.yaml
-bcnf train -c configs/runs/trajectory_TRF_large.yaml
-bcnf train -c configs/runs/videos_CNN_LSTM_large.yaml
+bcnf train -c {{BCNF_ROOT}}/configs/runs/trajectory_LSTM_small.yaml -f
+bcnf train -c {{BCNF_ROOT}}/configs/runs/trajectory_LSTM_large.yaml -f
+bcnf train -c {{BCNF_ROOT}}/configs/runs/trajectory_TRF_small.yaml -f
+bcnf train -c {{BCNF_ROOT}}/configs/runs/trajectory_TRF_large.yaml -f
+bcnf train -c {{BCNF_ROOT}}/configs/runs/videos_CNN_LSTM_large.yaml -f
